d6f45dd039cb1709c568b7c7685ac61eb057ad8e
Luke Kang <luke@seoulrobotics.org>
2021-07-27 11:12:54 +0900

    make final debian build script.

--- make_debian_pkg.py
+++ make_debian_pkg.py
@@ -3,6 +3,7 @@ import os
 import platform
 import subprocess
 import shutil
+import glob
 
 def run_command(args, shell=False):
     kargs = {'check': True}
@@ -23,68 +24,9 @@ def get_arch_name():
         return arch_dict[arch]
     return 'any'
 
-# https://linuxconfig.org/easy-way-to-create-a-debian-package-and-local-package-repository
-# def create_deb_package(root_dir, repo_url, cursor:str = ''):
-#     src_dir = component.get_source(repo_url, root_dir, cursor=cursor)
-
-#     os.chdir(src_dir)
-#     if not cursor:
-#         cmd_get_tag = 'git describe --tags --exact-match 2> /dev/null'
-#         cursor = subprocess.run(cmd_get_tag, shell=True).stdout
-
-#         if not cursor:
-#             cursor = '0.0.1' # set an arbitrary version number
-
-#     cursor = re.sub('[a-zA-Z]', '', cursor) # version should not have any alphabet.
-#     arch = install.get_arch_name()
-
-#     pkg_name = os.path.basename(src_dir)
-#     pkg_fullname = '_'.join([pkg_name, cursor, arch])
-#     stage_dir = os.path.join(src_dir, pkg_fullname)
-
-#     build_dir = install.build_cmake_project(src_dir)
-
-#     os.chdir(build_dir)
-#     install.run_command(['make', f'DESTDIR={stage_dir}', 'install'])
-
-#     # Note: some packages have different .so file names from their own names.
-#     #       e.g., LightGBM: lib_lightgbm.so
-#     #       Let's pray for luck in the case
-#     root_lib_dir = os.path.join(stage_dir, 'usr/local/lib')
-#     target_lib_path = os.path.join(root_lib_dir, f'lib{pkg_name}.so')
-
-#     if not os.path.exists(target_lib_path):
-#         lib_pattern = os.path.join(root_lib_dir, '**', f'lib{pkg_name}.so*')
-
-#         libs = glob.glob(lib_pattern, recursive=True)
-#         for source_lib in libs:
-#             if not os.path.islink(source_lib):
-#                 os.symlink(os.path.relpath(source_lib, root_lib_dir), target_lib_path)
-
-#     debinfo_dir = os.path.join(stage_dir, 'DEBIAN')
-#     os.makedirs(debinfo_dir)
-
-#     # use list to keep the items ordered
-#     debinfo = [
-#         ('Package', pkg_name),
-#         ('Version', cursor),
-#         ('Architecture', arch),
-#         ('Maintainer', 'seoulrobotics (support@seoulrobotics.org)'),
-#         ('Description', 'This package is not built by the authors but by seoulrobotics.')
-#     ]
-
-#     with open(os.path.join(debinfo_dir, 'control'), 'w') as fp:
-#         fp.writelines([f'{k}: {v}\n' for k, v in debinfo])
-
-#     os.chdir(src_dir)
-#     install.run_command(['dpkg-deb', '--build', pkg_fullname])
-
-#     shutil.move(os.path.join(src_dir, pkg_fullname + '.deb'), root_dir)
-
-#     return os.path.join(root_dir, pkg_fullname + '.deb')
-
-def create_deb_package(root_dir, repo_url, cursor:str = ''):
-    #run_command('pip3 install meson --user') # apt does not have a proper version of meson yet.
+def create_deb_package():
+    run_command('pip3 install meson --user') # apt does not have a proper version of meson yet.
+    
     src_dir = os.getcwd()
 
     # Read version
@@ -100,48 +42,33 @@ def create_deb_package(root_dir, repo_url, cursor:str = ''):
       elif line.find('VERSION_GIT_DATE') != -1:
         version = version + "."  + line.split(' ')[-1]
         break
+    # Prepare variables
     arch = get_arch_name()
     pkg_name = os.path.basename(src_dir)
     pkg_fullname = '_'.join([pkg_name, version, arch])
     stage_dir = os.path.join(src_dir, pkg_fullname)
     shutil.copytree(os.path.join(src_dir, 'debian_template'), stage_dir)
     temp_dir = os.path.join(src_dir, 'temp_install')
+    # Build
     run_command(['meson', 'setup', 'build',
-     '--buildtype=release',
+     '--buildtype=release', 
+     '-DPISTACHE_BUILD_EXAMPLES=false',
+     '-DPISTACHE_BUILD_TESTS=false',
+     '-DPISTACHE_BUILD_DOCS=false',
      f'--prefix={temp_dir}'
      ])
     run_command(['meson', 'compile', '-C build'])
-    shutil.copytree(os.path.join(stage_dir, 'include'), os.path.join(stage_dir, 'usr/local/include'))
-    shutil.copytree(os.path.join(stage_dir, 'lib/x86_64-linux-gnu'), os.path.join(stage_dir, 'usr/local/lib'))
-    #run_command(['meson', 'install', '-C build'])
-   
+    run_command(['meson', 'install', '-C build'])
+    # Make debian package folder
+    shutil.copytree(os.path.join(temp_dir, 'include'), os.path.join(stage_dir, 'usr/local/include/pistache'))
+    for item in os.listdir(os.path.join(temp_dir, 'lib/x86_64-linux-gnu')):
+      s = os.path.join(os.path.join(temp_dir, 'lib/x86_64-linux-gnu'), item)
+      d = os.path.join(os.path.join(stage_dir, 'usr/local/lib'), item)
+      if os.path.isdir(s) == False:
+        shutil.copy2(s, d, follow_symlinks=False) 
 
-    # source = "/parent/subdir"
-    # destination = "/parent/"
-    # files_list = os.listdir(source)
-    # for files in files_list:
-    #     shutil.move(files, destination)
-
-    # for filename in os.listdir(os.path.join(temp_dir, 'slave')):
-    #     shutil.move(os.path.join(temp_dir, 'slave', filename), os.path.join(temp_dir, filename))
-    # os.rmdir(temp_dir)
-    # # Note: some packages have different .so file names from their own names.
-    # #       e.g., LightGBM: lib_lightgbm.so
-    # #       Let's pray for luck in the case
-    # root_lib_dir = os.path.join(stage_dir, '/lib/x86_64-linux-gnu')
-    # target_lib_path = os.path.join(os.path.join(stage_dir, '/usr/local/lib'), f'lib{pkg_name}.so')
-
-    # if not os.path.exists(target_lib_path):
-    #     lib_pattern = os.path.join(root_lib_dir, '**', f'lib{pkg_name}.so*')
-
-    #     libs = glob.glob(lib_pattern, recursive=True)
-    #     for source_lib in libs:
-    #         if not os.path.islink(source_lib):
-    #             os.symlink(os.path.relpath(source_lib, root_lib_dir), target_lib_path)
-
+    # Update debian control file.
     debinfo_dir = os.path.join(stage_dir, 'DEBIAN')
-
-    # use list to keep the items ordered
     debinfo = [
         ('Package', pkg_name),
         ('Version', version),
@@ -149,15 +76,25 @@ def create_deb_package(root_dir, repo_url, cursor:str = ''):
         ('Maintainer', 'seoulrobotics (support@seoulrobotics.org)'),
         ('Description', 'This package is not built by the authors but by seoulrobotics.')
     ]
-
     with open(os.path.join(debinfo_dir, 'control'), 'w') as fp:
         fp.writelines([f'{k}: {v}\n' for k, v in debinfo])
+
+    # Replace version in pkgconfig file.
+    pkgconfig_file = os.path.join(stage_dir, 'usr/local/lib/pkgconfig/libpistache.pc')
+
+    fin = open(pkgconfig_file, "rt")
+    data = fin.read()
+    data = data.replace('#REPLACE_VER', version)
+    fin.close()
+    fin = open(pkgconfig_file, "wt")
+    fin.write(data)
+    fin.close()
+
     run_command(['dpkg-deb', '--build', pkg_fullname])
 
-    #shutil.move(os.path.join(src_dir, pkg_fullname + '.deb'), root_dir)
-
+    shutil.rmtree(temp_dir)
+    shutil.rmtree(stage_dir)
+    shutil.rmtree(os.path.join(src_dir, 'build'))
 
 if __name__ == '__main__':
-  cwd = os.getcwd()
-
-  deb_path = create_deb_package(cwd, 'repo_url', cursor='cursor')
+  deb_path = create_deb_package()
